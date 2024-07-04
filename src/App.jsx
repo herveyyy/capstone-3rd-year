e3a69a61240c8e66c53af64da518085efe8d4162
ReZeroKairu <keilanimaynard@gmail.com>
2024-07-04 22:43:32 +0800

    Added more

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
+import { Route, Routes } from "react-router-dom";
+import SignIn from "./pages/SignIn";
+import Home from "./pages/Home";
+import SignUp from "./pages/SignUp";
+import ResearcherDashboard from "./pages/Researcher/Dashboard";
+import PeerReviewerDashboard from "./pages/PeerReviewer/Dashboard";
+import AdminDashboard from "./pages/Admin/Dashboard";
+import AdminManuscript from "./pages/Admin/Manuscript";
+import Navbar from "./components/Navbar";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/admin/" element={<AdminDashboard />} />
+        <Route path="/admin/manuscript" element={<AdminManuscript />} />
+      </Routes>
+    </div>
+  );
 }
